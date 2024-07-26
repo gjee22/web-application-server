fb73ab025982898888e621101a12a43a38ba6622
Gayeon Jee <G.Jee@student.tudelft.nl>
2024-07-26 16:43:35 +0900

    요구 1- index.html 응답하기 구현하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,14 +30,24 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다        	
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	String dataString = br.readLine().split(" ")[1];
+            log.info("Received data: {}", dataString);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = getContent(dataString);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private byte[] getContent(String dataString) throws IOException {
+    	return switch (dataString) {		
+	    	case "/index.html" -> { yield Files.readAllBytes(Paths.get(new File("./webapp/index.html").toURI())); }
+			default -> { yield "Hello World".getBytes(); }
+		};
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
