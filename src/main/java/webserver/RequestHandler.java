3b2d3ba7d07cea37d2aa513cd7400e53a3d99855
gjee22 <G.Jee@student.tudelft.nl>
2024-07-27 15:52:16 +0900

    요구사항 2: GET 방식으로 회원가입하기 구현하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.awt.desktop.UserSessionEvent;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,10 +10,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
 
 
 
@@ -20,7 +26,7 @@ public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
-
+    
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
@@ -46,8 +52,22 @@ public class RequestHandler extends Thread {
     private byte[] getContent(String dataString) throws IOException {
     	return switch (dataString) {		
 	    	case "/index.html" -> { yield Files.readAllBytes(Paths.get(new File("./webapp/index.html").toURI())); }
-			default -> { yield "Hello World".getBytes(); }
+	    	case "/user/form.html" -> { yield Files.readAllBytes(Paths.get(new File("./webapp/user/form.html").toURI())); }
+			default -> { 
+				if (dataString.startsWith("/user/create?")) yield register(dataString.split("\\?")[1]); 
+				yield "Hello World".getBytes(); 
+			}
 		};
+    }
+    
+    private byte[] register(String dataString) throws IOException {
+    	Map<String, String> userInfo = HttpRequestUtils.parseQueryString(dataString);
+    	User user = new User(userInfo.get("userId"), userInfo.get("password"), userInfo.get("name"), userInfo.get("email"));
+    	DataBase.addUser(user);
+    	for (User u : DataBase.findAll()) {
+    		System.out.println(u);
+    	}
+    	return Files.readAllBytes(Paths.get(new File("./webapp/user/form.html").toURI()));
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
